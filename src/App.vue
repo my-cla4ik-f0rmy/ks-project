a22df18fd3cc2bf36bb89b7e24ff49e9ee4dadfc
my-cla4ik-f0rmy <retiree-frets-0r@icloud.com>
2023-06-01 14:20:23 +0400

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,13 @@ import TextInput from "./components/TextInput.vue"
 import Menu from "./components/Menu.vue"
 import Size from "./components/size.vue"
 import Sketch from "./components/Sketch.vue"
-
 </script>
 
 <template>
   <div class="main">
     <div class="name">Шаблонус</div>
     <Menu title="Шаблон" idS="Menu"></Menu>
-    <Size  title="Тип" idS="Size"></Size>
+    <Size title="Тип" idS="Size"></Size>
     <TextInput title="Заголовок" idS="title"></TextInput>
     <TextInput title="Блок №1" idS="info1"></TextInput>
     <TextInput title="Блок №2" idS="info2"></TextInput>
@@ -36,7 +35,7 @@ import Sketch from "./components/Sketch.vue"
 @use "@material/button/styles.scss";
 
 
-.name{
+.name {
   font-size: 35px;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: 'https://github.com/my-cla4ik-f0rmy/ks-project',
   plugins: [vue()],
   server: {
     host: true
